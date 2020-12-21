3557ad321af1d04a77a7186e2b30213f5aad9744
Arief Muhammad Lubis <lubisarieff@gmail.com>
2020-12-21 21:44:44 +0700

    Add project files.

---
+++ StudiKasus/Data/StudiKasusContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudiKasus.Models;
+
+namespace StudiKasus.Data
+{
+    public class StudiKasusContext : DbContext
+    {
+        public StudiKasusContext (DbContextOptions<StudiKasusContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudiKasus.Models.User> User { get; set; }
+    }
+}
---
+++ StudiKasus/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudiKasus.Models
+{
+	public class User
+	{
+		public int Id { get; set; }
+		[Required]
+		public string UserName { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[Required]
+		public string Email { get; set; }
+		public string Status { get; set; }
+		public string Phone { get; set; }
+	}
+}
